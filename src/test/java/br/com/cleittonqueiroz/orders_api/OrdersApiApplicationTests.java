8df1cf535d2961fddcd3a32db1feca852becf72c
Cleiton Queiroz <cleiton.marques2012@gmail.com>
2026-05-11 15:28:13 -0300

    feat: Configuração inicial do projeto Sprint Boot

---
+++ src/test/java/br/com/cleittonqueiroz/orders_api/OrdersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cleittonqueiroz.orders_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
