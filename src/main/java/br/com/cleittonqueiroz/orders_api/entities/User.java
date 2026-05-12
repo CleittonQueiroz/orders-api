ae73134c9f18da69e18f93dfa34ec06c33c17474
Cleiton Queiroz <cleiton.marques2012@gmail.com>
2026-05-12 11:02:02 -0300

    feat: configure H2 database and User JPA mapping

--- src/main/java/br/com/cleittonqueiroz/orders_api/entities/User.java
+++ src/main/java/br/com/cleittonqueiroz/orders_api/entities/User.java
@@ -1,10 +1,18 @@
 package br.com.cleittonqueiroz.orders_api.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable {
+@Entity
+@Table(name = "tb_user")
 
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
